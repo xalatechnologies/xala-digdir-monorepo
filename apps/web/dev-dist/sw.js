a298411e55edcfc6b6da0f56ae6b812467d560ec
xalatechnologies <info@xala.no>
2026-01-18 15:31:57 +0100

    Fix Docker monorepo: copy tsconfig.json and run from workspace root

--- apps/web/dev-dist/sw.js
+++ apps/web/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ebf8facd'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.3goq8buf7t"
+    "revision": "0.sdhn74tj73o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
